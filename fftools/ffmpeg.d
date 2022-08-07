689b4659136e53e9ce80e5a14259082ddf6d2ded
moinmoin2 <48869696+moinmoin2@users.noreply.github.com>
2022-08-07 18:21:06 +0200

    Initial commit

---
+++ fftools/ffmpeg.d
@@ -0,0 +1,36 @@
+fftools/ffmpeg.o: fftools/ffmpeg.c config.h libavformat/avformat.h \
+ libavcodec/codec.h libavutil/avutil.h libavutil/common.h \
+ libavutil/attributes.h libavutil/macros.h libavutil/avconfig.h \
+ libavutil/mem.h libavutil/avutil.h libavutil/version.h libavutil/error.h \
+ libavutil/rational.h libavutil/mathematics.h libavutil/intfloat.h \
+ libavutil/log.h libavutil/pixfmt.h libavutil/hwcontext.h \
+ libavutil/buffer.h libavutil/frame.h libavutil/channel_layout.h \
+ libavutil/dict.h libavutil/samplefmt.h libavutil/log.h \
+ libavutil/pixfmt.h libavutil/rational.h libavutil/samplefmt.h \
+ libavcodec/codec_id.h libavcodec/version_major.h libavcodec/codec_par.h \
+ libavutil/channel_layout.h libavcodec/codec_id.h libavcodec/defs.h \
+ libavcodec/packet.h libavutil/attributes.h libavutil/buffer.h \
+ libavutil/dict.h libavutil/version.h libavformat/avio.h \
+ libavformat/version_major.h libavformat/version.h \
+ libavformat/version_major.h libavdevice/avdevice.h \
+ libavdevice/version_major.h libavdevice/version.h libavutil/opt.h \
+ libswresample/swresample.h libavutil/frame.h \
+ libswresample/version_major.h libswresample/version.h \
+ libswresample/version_major.h libavutil/parseutils.h libavutil/fifo.h \
+ libavutil/internal.h libavutil/timer.h libavutil/libm.h \
+ libavutil/intreadwrite.h libavutil/bswap.h libavutil/display.h \
+ libavutil/mathematics.h libavutil/pixdesc.h libavutil/avstring.h \
+ libavutil/libm.h libavutil/imgutils.h libavutil/pixdesc.h \
+ libavutil/timestamp.h libavutil/bprint.h libavutil/avstring.h \
+ libavutil/time.h libavutil/thread.h libavutil/threadmessage.h \
+ libavcodec/mathops.h libavutil/common.h libavcodec/version.h \
+ libavcodec/version_major.h libavformat/os_support.h \
+ libavfilter/avfilter.h libavfilter/version_major.h libavfilter/version.h \
+ libavfilter/version_major.h libavfilter/buffersrc.h \
+ libavfilter/avfilter.h libavfilter/buffersink.h fftools/ffmpeg.h \
+ fftools/cmdutils.h libavcodec/avcodec.h libavcodec/codec.h \
+ libavcodec/codec_desc.h libavcodec/codec_par.h libavcodec/defs.h \
+ libavcodec/packet.h libavcodec/version.h libswscale/swscale.h \
+ libswscale/version_major.h libswscale/version.h fftools/sync_queue.h \
+ libavformat/avio.h libavcodec/bsf.h libavutil/eval.h \
+ libavutil/avassert.h
---
+++ fftools/ffmpeg_hw.d
@@ -0,0 +1,26 @@
+fftools/ffmpeg_hw.o: fftools/ffmpeg_hw.c libavutil/avstring.h \
+ libavutil/attributes.h libavutil/version.h libavutil/macros.h \
+ libavutil/avconfig.h libavutil/pixdesc.h libavutil/pixfmt.h \
+ libavfilter/buffersink.h libavfilter/avfilter.h libavutil/attributes.h \
+ libavutil/avutil.h libavutil/common.h libavutil/mem.h libavutil/avutil.h \
+ libavutil/error.h libavutil/rational.h libavutil/mathematics.h \
+ libavutil/intfloat.h libavutil/log.h libavutil/buffer.h libavutil/dict.h \
+ libavutil/frame.h libavutil/buffer.h libavutil/channel_layout.h \
+ libavutil/dict.h libavutil/samplefmt.h libavutil/log.h \
+ libavutil/samplefmt.h libavutil/pixfmt.h libavutil/rational.h \
+ libavfilter/version_major.h libavfilter/version.h libavutil/version.h \
+ libavfilter/version_major.h fftools/ffmpeg.h config.h fftools/cmdutils.h \
+ libavcodec/avcodec.h libavcodec/codec.h libavutil/hwcontext.h \
+ libavutil/frame.h libavcodec/codec_id.h libavcodec/version_major.h \
+ libavcodec/codec_desc.h libavcodec/codec_id.h libavcodec/codec_par.h \
+ libavutil/channel_layout.h libavcodec/defs.h libavcodec/packet.h \
+ libavcodec/version_major.h libavcodec/version.h libavfilter/avfilter.h \
+ libavformat/avformat.h libavcodec/codec.h libavcodec/codec_par.h \
+ libavcodec/defs.h libavcodec/packet.h libavformat/avio.h \
+ libavformat/version_major.h libavformat/version.h \
+ libavformat/version_major.h libswscale/swscale.h \
+ libswscale/version_major.h libswscale/version.h fftools/sync_queue.h \
+ libavformat/avio.h libavcodec/bsf.h libavutil/eval.h libavutil/fifo.h \
+ libavutil/thread.h libavutil/threadmessage.h libswresample/swresample.h \
+ libswresample/version_major.h libswresample/version.h \
+ libswresample/version_major.h
